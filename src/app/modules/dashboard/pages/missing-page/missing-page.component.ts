da231d5faebf4b8e9751e00aa2dccac7d8a57ff7
DkChriss <chrisshuayhua@hotmail.com>
2025-05-28 15:47:36 -0400

    Create components

--- src/app/modules/dashboard/pages/missing-page/missing-page.component.ts
+++ src/app/modules/dashboard/pages/missing-page/missing-page.component.ts
@@ -7,14 +7,15 @@ import { BehaviorSubject, combineLatest, debounceTime, distinctUntilChanged, map
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { QuillModule } from 'ngx-quill';
-import { Missing, MissingStore, MissingUpdate, StatusMissingEnum } from '../../models/missing';
+import { StatusMissingEnum } from '../../models/missing';
+import { MissingRegisterFormComponent } from '../../components/missing-register-form/missing-register-form.component';
 
 @Component({
     selector: 'app-missing-page',
@@ -42,9 +43,6 @@ export class MissingPageComponent implements OnInit, OnDestroy {
     displayedColumns: string[] = ['name', 'location', 'phone', 'status_missing', 'actions'];
     configForm: UntypedFormGroup;
     isLoading = true
-    isEditMode = false
-    selectedMissing: Missing | null = null
-    method = 'store'
     form = {
         submitted: false,
         submitting: false,
@@ -74,7 +72,6 @@ export class MissingPageComponent implements OnInit, OnDestroy {
     pageNumber$ = new BehaviorSubject<number>(1);
     searchBy$ = new BehaviorSubject<string>('');
     totalItems = 0
-    missing: any
 
     missingList$ = combineLatest([
         this.pageSize$,
@@ -91,7 +88,6 @@ export class MissingPageComponent implements OnInit, OnDestroy {
         ).pipe(
             tap((res: any) => {
                 this.totalItems = res.total;
-                this.missing = res.data
                 this.isLoading = false
             }),
             map((res: any) => res.data)
@@ -99,6 +95,7 @@ export class MissingPageComponent implements OnInit, OnDestroy {
     )
 
     constructor(
+        private dialog: MatDialog,
         private _missingService: MissingService,
         private _fuseConfirmationService: FuseConfirmationService,
         private _formBuilder: UntypedFormBuilder,
@@ -133,131 +130,22 @@ export class MissingPageComponent implements OnInit, OnDestroy {
 
     }
 
-    openStore(): void {
-        this.form.formGroup.reset()
-        this.closeDetails();
-        this.isEditMode = true;
-        let newMissing: Missing = {
-            id: this.missing[0]["id"],
-            user_id: 1,
-            name: 'name',
-            last_name: 'last_name',
-            age: 0,
-            gender: 'gender',
-            description: 'description',
-            birthdate: new Date(),
-            disappearance_date: new Date(),
-            place_of_disappearance: 'place of disappearance',
-            status_missing: StatusMissingEnum.pending,
-            characteristics: "characteristics",
-            reporter_name: "reporter_name",
-            reporter_phone: "reporter_phone",
-            location: {
-                latitude: "1,203923",
-                longitude: "1,2309832"
-            }
-
-        }
-        this.method = 'store'
-        this.selectedMissing = newMissing
-
-    }
-
     store(): void {
-        this.form.submitted = true
-        if (this.form.formGroup.valid) {
-            this.form.submitting = true
-            const newMissing: MissingStore = this.form.formGroup.getRawValue();
-            this._missingService.store(newMissing).subscribe({
-                next: (resp: any) => {
-                    this.missingTable.reload.next();
-                    this.closeDetails();
-                },
-                error: (error) => {
-                    console.log(error)
-                }
-            })
-        }
+        const dialogRef = this.dialog.open(MissingRegisterFormComponent, {
+            width: '70%',
+            height: 'auto',
+            disableClose: false
+        })
+
+        dialogRef.afterClosed().subscribe(result => {
+            this.missingTable.reload.next();
+        })
     }
 
+    update(): void {
 
-    closeDetails(): void {
-        this.selectedMissing = null
-        this.isEditMode = false
     }
 
-    toggleDetails(missing: Missing): void {
-        this.isEditMode = !this.isEditMode
-        if (this.selectedMissing?.id == missing.id) {
-            this.selectedMissing = null
-        } else {
-            this.selectedMissing = missing
-            this._missingService.show(missing.id).subscribe({
-                next: (resp: any) => {
-                    this.method = 'update'
-                    this.isEditMode = true;
-                    this.form.formGroup.patchValue({
-                        user_id: resp.data.user_id,
-                        name: resp.data.name,
-                        last_name: resp.data.last_name,
-                        age: resp.data.age,
-                        gender: resp.data.gender,
-                        description: resp.data.description,
-                        birthdate: resp.data.birthdate,
-                        disappearance_date: resp.data.disappearance_date,
-                        place_of_disappearance: resp.data.place_of_disappearance,
-                        status_missing: resp.data.status_missing,
-                        characteristics: resp.data.characteristics,
-                        reporter_name: resp.data.reporter_name,
-                        reporter_phone: resp.data.reporter_phone,
-                        location: resp.data.location
-                    }, { emitEvent: false })
-                },
-                error: (error) => {
-                    console.log(error)
-                }
-            })
-        }
-    }
-
-    update(id: number): void {
-        this.form.submitted = true
-        if (this.form.formGroup.valid) {
-            this.form.submitting = true
-            const missingUpdate: MissingUpdate = this.form.formGroup.getRawValue()
-            this._missingService.update(id, missingUpdate).subscribe({
-                next: (resp: any) => {
-                    this.missingTable.reload.next();
-                    this.closeDetails();
-                },
-                error: (error) => {
-                    console.log(error)
-                }
-            })
-        }
-    }
-
-    cancelEdit() {
-        if (this.selectedMissing) {
-            this.form.formGroup.patchValue({
-                user_id: this.selectedMissing.user_id,
-                name: this.selectedMissing.name,
-                last_name: this.selectedMissing.last_name,
-                age: this.selectedMissing.age,
-                gender: this.selectedMissing.gender,
-                description: this.selectedMissing.description,
-                birthdate: this.selectedMissing.birthdate,
-                disappearance_date: this.selectedMissing.disappearance_date,
-                place_of_disappearance: this.selectedMissing.place_of_disappearance,
-                status_missing: this.selectedMissing.status_missing,
-                characteristics: this.selectedMissing.characteristics,
-                reporter_name: this.selectedMissing.reporter_name,
-                reporter_phone: this.selectedMissing.reporter_phone,
-                location: this.selectedMissing.location
-            }, { emitEvent: false })
-        }
-        this.isEditMode = false;
-    }
 
     delete(id: number) {
         if (id) {
@@ -268,7 +156,6 @@ export class MissingPageComponent implements OnInit, OnDestroy {
                     this._missingService.delete(id).subscribe({
                         next: (resp) => {
                             this.missingTable.reload.next()
-                            this.method = this.totalItems === 0 ? "store" : "update"
                         }, error: (error) => {
                             console.log(error)
                         }
